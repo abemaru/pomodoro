340b8fef9b33a469afeca2563cd55f6a92f4fb4f
abemaru <ch4nge.lightening@gmail.com>
2022-10-24 08:59:44 +0900

    add all settings

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -9,14 +9,7 @@ type counterSetting = {
 	longBreakAfter:number
 }
 
-
-
-interface CounterProps {
-  start: number;
-}
-
 function initCounterSettings(): counterSetting {
-	
 	return {
 		'workingMinutes': 25,
 		'breakMinutes': 5,
@@ -30,25 +23,103 @@ function initCounterSettings(): counterSetting {
 export default function Counter() {
 	const setting = initCounterSettings();
   const [workingMinutes, setWorkingMinutes] = useState(setting.workingMinutes);
+	const [breakMinutes, setBreakMinutes] = useState(setting.breakMinutes);
+	const [longBreakMinutes, setLongBreakMinutes] = useState(setting.longBreakMinutes);
+	const [breakAfter, setBreakAfter] = useState(setting.breakAfter);
+	const [longBreakAfter, setLongBreakAfter] = useState(setting.longBreakAfter);
 	
   return (
-    <div class="flex justify-center">
-			<div class="py-3 pr-3 text(white)">
-				Mob Timer
+		<div>
+  		<div class="flex justify-center">
+				<div class="py-3 pr-3 text(white)">
+					working Minutes
+				</div>
+				<Button
+					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300)"
+					onClick={() => setWorkingMinutes(workingMinutes - 1)}>-
+				</Button>
+				<div class="px-4 py-2 text(2xl white) border-y-1">
+    	    <p>{workingMinutes}</p>
+				</div>
+				<Button
+					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300)"
+					onClick={() => setWorkingMinutes(workingMinutes + 1)}>+</Button>
+				<div class="py-3 pl-3 text(white)">
+					minutes
+				</div>
+  		</div>
+			<div class="flex justify-center">
+				<div class="py-3 pr-3 text(white)">
+					break minutes
+				</div>
+				<Button
+					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300)"
+					onClick={() => setBreakMinutes(breakMinutes - 1)}>-
+				</Button>
+				<div class="px-4 py-2 text(2xl white) border-y-1">
+    	    <p>{breakMinutes}</p>
+				</div>
+				<Button
+					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300)"
+					onClick={() => setBreakMinutes(breakMinutes + 1)}>+</Button>
+				<div class="py-3 pl-3 text(white)">
+					minutes
+				</div>
 			</div>
-			<Button
-				class="rounded-l-lg bg-orange px-4 py-2 text(gray-300)"
-				onClick={() => setWorkingMinutes(workingMinutes - 1)}>-
-			</Button>
-			<div class="px-4 py-2 text(2xl white) border-y-1">
-        <p>{workingMinutes}</p>
+			<div class="flex justify-center">
+				<div class="py-3 pr-3 text(white)">
+					long break minutes
+				</div>
+				<Button
+					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300)"
+					onClick={() => setLongBreakMinutes(longBreakMinutes - 1)}>-
+				</Button>
+				<div class="px-4 py-2 text(2xl white) border-y-1">
+    	    <p>{longBreakMinutes}</p>
+				</div>
+				<Button
+					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300)"
+					onClick={() => setLongBreakMinutes(longBreakMinutes + 1)}>+</Button>
+				<div class="py-3 pl-3 text(white)">
+					minutes
+				</div>
 			</div>
-			<Button
-				class="rounded-r-lg bg-orange px-4 py-2 text(gray-300)"
-				onClick={() => setWorkingMinutes(workingMinutes + 1)}>+</Button>
-			<div class="py-3 pl-3 text(white)">
-				minutes
+			<div class="flex justify-center">
+				<div class="py-3 pr-3 text(white)">
+					break after 
+				</div>
+				<Button
+					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300)"
+					onClick={() => setBreakAfter(breakAfter - 1)}>-
+				</Button>
+				<div class="px-4 py-2 text(2xl white) border-y-1">
+    	    <p>{breakAfter}</p>
+				</div>
+				<Button
+					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300)"
+					onClick={() => setBreakAfter(breakAfter + 1)}>+</Button>
+				<div class="py-3 pl-3 text(white)">
+					times
+				</div>
 			</div>
-    </div>
+			<div class="flex justify-center">
+				<div class="py-3 pr-3 text(white)">
+					long break after 
+				</div>
+				<Button
+					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300)"
+					onClick={() => setLongBreakAfter(longBreakAfter - 1)}>-
+				</Button>
+				<div class="px-4 py-2 text(2xl white) border-y-1">
+    	    <p>{longBreakAfter}</p>
+				</div>
+				<Button
+					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300)"
+					onClick={() => setLongBreakAfter(longBreakAfter + 1)}>+</Button>
+				<div class="py-3 pl-3 text(white)">
+					times
+				</div>
+			</div>
+		</div>
   );
 }
