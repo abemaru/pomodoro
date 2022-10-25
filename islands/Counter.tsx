2080709e5423628c9f00975fed9f77cb2c335de1
abemaru <ch4nge.lightening@gmail.com>
2022-10-25 09:04:06 +0900

    set width

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -83,93 +83,93 @@ export default function Counter() {
 
   return (
 		<div>
-  		<div class="flex justify-center">
-				<div class="py-3 pr-3 text(white)">
+  		<div class="flex justify-center mb-1">
+				<div class="text(white) w-40">
 					working Minutes
 				</div>
 				<Button
-					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300)"
-					onClick={handleWorkingMinutesMinus}>-
+					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300) w-10"
+		 			onClick={handleWorkingMinutesMinus}>-
 				</Button>
-				<div class="px-4 py-2 text(2xl white) border-y-1">
+				<div class="text(2xl white) border-y-1 w-12">
     	    <p>{counters.workingMinutes}</p>
 				</div>
 				<Button
-					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300)"
+					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300) w-10"
 					onClick={handleWorkingMinutesPlus}>+</Button>
-				<div class="py-3 pl-3 text(white)">
+				<div class="text(white) w-40">
 					minutes
 				</div>
   		</div>
-			<div class="flex justify-center">
-				<div class="py-3 pr-3 text(white)">
+			<div class="flex justify-center mb-1">
+				<div class="text(white) w-40">
 					break minutes
 				</div>
 				<Button
-					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300)"
+					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300) w-10"
 					onClick={handleBreakMinutesMinus}>-
 				</Button>
-				<div class="px-4 py-2 text(2xl white) border-y-1">
+				<div class="text(2xl white) border-y-1 w-12">
     	    <p>{counters.breakMinutes}</p>
 				</div>
 				<Button
-					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300)"
+					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300) w-10"
 					onClick={handleBreakMinutesPlus}>+</Button>
-				<div class="py-3 pl-3 text(white)">
+				<div class="text(white) w-40">
 					minutes
 				</div>
 			</div>
-			<div class="flex justify-center">
-				<div class="py-3 pr-3 text(white)">
+			<div class="flex justify-center mb-1">
+				<div class="text(white) w-40">
 					long break minutes
 				</div>
 				<Button
-					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300)"
+					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300) w-10"
 					onClick={handleLongBreakMinutesMinus}>-
 				</Button>
-				<div class="px-4 py-2 text(2xl white) border-y-1">
+				<div class="text(2xl white) border-y-1 w-12">
     	    <p>{counters.longBreakMinutes}</p>
 				</div>
 				<Button
-					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300)"
+					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300) w-10"
 					onClick={handleLongBreakMinutesPlus}>+</Button>
-				<div class="py-3 pl-3 text(white)">
+				<div class="text(white) w-40">
 					minutes
 				</div>
 			</div>
-			<div class="flex justify-center">
-				<div class="py-3 pr-3 text(white)">
+			<div class="flex justify-center mb-1">
+				<div class="text(white) w-40">
 					break after 
 				</div>
 				<Button
-					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300)"
+					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300) w-10"
 					onClick={handleBreakAfterMinus}>-
 				</Button>
-				<div class="px-4 py-2 text(2xl white) border-y-1">
+				<div class="text(2xl white) border-y-1 w-12">
     	    <p>{counters.breakAfter}</p>
 				</div>
 				<Button
-					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300)"
+					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300) w-10"
 					onClick={handleBreakAfterPlus}>+</Button>
-				<div class="py-3 pl-3 text(white)">
+				<div class="text(white) w-40">
 					times
 				</div>
 			</div>
-			<div class="flex justify-center">
-				<div class="py-3 pr-3 text(white)">
+			<div class="flex justify-center mb-1">
+				<div class="text(white) w-40">
 					long break after 
 				</div>
 				<Button
-					class="rounded-l-lg bg-orange px-4 py-2 text(gray-300)"
+					class="rounded-l-lg bg-orange text(gray-300) w-10"
 					onClick={handleLongBreakAfterMinus}>-
 				</Button>
-				<div class="px-4 py-2 text(2xl white) border-y-1">
+				<div class="text(2xl white) border-y-1 w-12">
     	    <p>{counters.longBreakAfter}</p>
 				</div>
 				<Button
-					class="rounded-r-lg bg-orange px-4 py-2 text(gray-300)"
+					class="rounded-r-lg bg-orange text(gray-300) w-10"
 					onClick={handleLongBreakAfterPlus}>+</Button>
-				<div class="py-3 pl-3 text(white)">
+				<div class="text(white) w-40">
 					times
 				</div>
 			</div>
